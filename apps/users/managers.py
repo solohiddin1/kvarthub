1b95c6d97df184b9992339e358a2d49e451fe6f3
Solohiddin <sirojiddinovsolohiddin961@gmail.com>
2025-12-08 18:04:53 +0500

    example

--- apps/users/managers.py
+++ apps/users/managers.py
@@ -24,33 +24,3 @@ class MyUserManager(BaseUserManager):
         user = self.create_user(email, password, **extra_fields)
 
         return user
-
-# class CustomUserManager(BaseUserManager):
-#     def create_user(self, phone_number=None, email=None,password = None ,**extra_fields):
-#         if not username:
-#             raise ValueError('Phone_number maydoni bo`lishi kerak emas!')
-#         # phone_number = self.normalize_phone_number(phone_number)
-#         user = self.model(phone_number=phone_number, email=email, **extra_fields)
-        
-#         user.set_password(password or '123456')
-#         user.save(using=self._db)
-#         return user
-
-
-#     def create_superuser(self, username, password=None, **extra_fields):
-#         extra_fields.setdefault("is_staff", True)
-#         extra_fields.setdefault("is_superuser", True)
-#         extra_fields.setdefault("is_active", True)
-
-#         if extra_fields.get("is_staff") is not True:
-#             raise ValueError("Superuser must have is_staff=True.")
-#         if extra_fields.get("is_superuser") is not True:
-#             raise ValueError("Superuser must have is_superuser=True.")
-
-#         user = self.create_user(username, password, **extra_fields)
-
-#         # superuser uchun default ADMIN roli berish
-#         from apps.users.models import UserRole
-#         UserRole.objects.get_or_create(user=user, role="ADMIN")
-
-#         return user
