64f1f55b1887955810941a4120f2f7e93e564135
Solohiddin <sirojiddinovsolohiddin961@gmail.com>
2025-12-22 02:39:22 +0500

    some changes

--- back/apps/listings/urls.py
+++ back/apps/listings/urls.py
@@ -5,7 +5,8 @@ from apps.listings.views import (
     ListingsListView,
     ListingRetrieveView,
     ListingUpdateView,
-    ListingDestroyView
+    ListingDestroyView,
+    MyListingsListView
 )
 
 urlpatterns = [
@@ -14,12 +15,15 @@ urlpatterns = [
     
     # Create a new listing
     path('create/', ListingCreateView.as_view(), name='listing_create'),
+
+    # my listings
+    path('my-listings/', MyListingsListView.as_view(), name='my_listings'),
     
     # Retrieve a listing by ID
     path('listings/<int:id>/', ListingRetrieveView.as_view(), name='listing_retrieve'),
     
     # Update a listing by ID
-    path('listings/<int:id>/update/', ListingUpdateView.as_view(), name='listing_update'),
+    path('<int:id>/update/', ListingUpdateView.as_view(), name='listing_update'),
     
     # Delete a listing by ID
     path('listings/<int:id>/delete/', ListingDestroyView.as_view(), name='listing_delete'),
